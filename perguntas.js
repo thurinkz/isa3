e1127c742d0e66ff26d48880f4704a37fe5ff216
Arthur Silva <168222534+thurinkz@users.noreply.github.com>
2024-11-25 16:28:51 -0300

    isa3

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasilia'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz gostar em Inglês?',
+    'Gostar em ingles é like'
+)
